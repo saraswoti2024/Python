702ba7c75a835451d968d936ed9ca326919eb69d
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-01-09 14:24:08 +0545

    day4

---
+++ Day4/Operator.py
@@ -0,0 +1,88 @@
+#operator= symbol that perform operation
+
+#1. arithmetic operator(+,-<,/)
+
+# num1 = 13
+# num2 = 2
+
+# #addition
+# print(num1+num2)
+
+# #subtraction
+# print(num1-num2)
+
+# #multiplication
+# print(num1*num2)
+
+# #division
+# print(num1/num2)
+
+# #modulus
+# print(num1%num2)
+
+# #floor 
+# print(num1//num2)
+
+# #power
+# print(num1**num2)
+
+# #2.Relation/comparison operator(<,>,==,<=,>=,!=)
+# a = 12
+# b = 15
+
+# print(a<b)
+# print(a>b)
+# print(a==b)
+# print(a<=b)
+# print(a>=b)
+# print(a!=b)
+
+# #3.assignment operator(=,+=,-=,//=,*=,**=)
+
+# a = 4
+# a += 1
+# a += 2
+# print(a)
+# a -= 2
+
+#4. logical operator (and,or,not)
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+# print(not(a))
+
+#5. membership operator(in,not in)
+# in -> only string
+# a = "my name"
+# b = 15
+# print("my" in a)
+# print("my" not in a)
+
+#6. identity operator -> memory address hererw equal xa ki xaina vanxa
+# a = [1,2,3]
+# b = [1,2,3]
+# print(id(a)) 
+# print(id(b))
+# print(a is b)
+# print(a==b) #value matrai herxa
+# print(15 is not b)
+
+#7. bit wise operator-> binary operation hunxa 12-> 1101 , 15-> 1111
+
+# and operator -> & (both true-> true, both false-> false, onefonet,onetonef-> false)
+a = 20
+b = 15
+print(a & b) #1101+1111
+
+#or operator -> | (one true-> true, both true-> true , both false-> false)
+print(a | b)
+
+#negotation operator (~) -> 2's complement garxa
+print(~(a)) #20-> -21, #13-> -14
+
+#xor
+#different-> true , same-> false
+print(a ^ b)
+
+
---
+++ Day4/input.py
@@ -0,0 +1,17 @@
+#bydefault input will be of string
+a = input("Enter your name: ")
+print(a)
+print(type(a))
+
+#data conversion by default python int>float>string
+a = 15
+b = 12.35
+print(type(a+b))
+
+#type casting-> hamle
+number = int(input("enter number: "))
+print(number)
+print(type(number))
+
+c = str(a+b)
+print(type(c))
