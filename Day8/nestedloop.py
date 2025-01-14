3e9c86dfac8e2e530e670ee5120fc5c6e19eb7db
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-01-14 21:48:28 +0545

    day8

---
+++ Day8/nestedloop.py
@@ -0,0 +1,14 @@
+#printing pattern
+
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * *
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("*",end=" ")
+#     print()
+
+
+#2
---
+++ Day8/pattern3.py
@@ -0,0 +1,17 @@
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+#process1 mylogic
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print("*",end=" ")
+#     print()
+
+#hererw gareko logic
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
