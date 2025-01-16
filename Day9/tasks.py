66793fd6a976438b4d96e9723ae1f61cbc118518
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-01-16 12:13:14 +0545

    tasks_added

---
+++ Day9/tasks.py
@@ -0,0 +1,57 @@
+#Write a Python function called print_square that takes a number as an argument and prints the square of that number
+
+
+# def print_square(num):
+#     print(f"square of {num}: {num**num}")
+
+# print_square(5)
+
+#Write a Python function called print_list_elements that takes a list as an argument and prints each element in the list one by one 
+
+# def print_list_elements(list1):
+#     for element in list1:
+#         print(element)
+    
+
+# list1 = [1,2,"sara",True]
+# print_list_elements(list1)
+
+
+# Write a Python function called multiply_by_two that takes a number as an argument and prints the number multiplied by 2
+
+# def multiply_by_two(num):
+#     print(f"{num}*2 = {num*2}")
+
+# multiply_by_two(2)
+
+#make a calculator by using function where each operator has its own function
+
+# def add(num1,num2):
+#     print(num1+num2)
+
+# def sub(num1,num2):
+#     print(num1-num2)
+
+# def mul(num1,num2):
+#     print(num1*num2)
+
+# def div(num1,num2):
+#     print(num1/num2)
+
+# def mod(num1,num2):
+#     print(num1%num2)
+
+# num1 = float(input("num1: "))
+# op = (input("op: "))
+# num2 = float(input("num2: "))
+
+# if op=='+':
+#     add(num1,num2)
+# elif op =='-':
+#     sub(num1,num2)
+# elif op=='*':
+#     mul(num1,num2)
+# elif op=='/':
+#     div(num1,num2)
+# elif op=='%':
+#     mod(num1,num2)
