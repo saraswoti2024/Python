2cae12a0a88dd671dad94e0ebe2031664230fae9
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-01-18 21:01:04 +0545

    day11

---
+++ Day11/tasks.py
@@ -0,0 +1,33 @@
+import math
+#total no of can required to paint the wall
+
+# def cans(height,weight,coverage=7):
+#         areas1 = areas(height=height,weight=weight)
+#         total = areas1/coverage
+#         return total
+
+# def areas(height,weight):
+#             area = height*weight
+#             return area
+
+# height = float(input("enter height: "))
+# weight = float(input("Enter weight: "))
+# # areas(height,weight)
+# total_Cans = cans(height,weight)
+# print(f"total can required with {height},{weight} is {math.ceil(total_Cans)}")
+
+#to check prime or not
+# def prime_check(number):
+#     is_prime = True
+#     for i in range(2,number):
+    
+#         if(number%i==0):
+#             is_prime=False
+
+#     if is_prime:
+#         print("prime number")
+#     else:
+#         print("not prime")
+
+# number = int(input("enter num: "))
+# prime_check(number)
