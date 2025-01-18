14b63998fed186dd5d5298308e8aa3dfd2677bfa
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-01-18 22:40:38 +0545

    added

--- Day3/dict.py
+++ Day3/dict.py
@@ -1,4 +1,4 @@
-##dict is mutable
+##dict is mutable keys are immutable only values are mutable
 
 # dicts = {}
 # print(dicts)
@@ -46,7 +46,6 @@
 # print(type(l2))
 
 #nested dictionary
-
 # d1 = {
 #     'emp' : {'name' : 'sara',
 #              'age' : 50},
@@ -58,17 +57,59 @@
 # print(d1)
 # print(d1['emp']['age'])
 
-
+#for multiple deleting,popping key-value use loop
 #removing using pop
 # person = {'name': 'Sarah',
 #            'age': 35, 
 #            'city': 'Miami', 
 #            'job': 'Lawyer'}
-# person.pop('age')
-# person.clear()
+
+# #adding list values
+# person['numbers'] = {123,345,"sara"}
+
+# #nested loop adding bahirw bata
+# person['fruits']= {'name':5,'banana':3,'mango':10}
+
+# # person.pop('age')
+# person.pop('name')
+# person['fruits'].pop('mango') #for nested key-value pop
+# # person.clear() #empty hunxa purai dict
+# print(person)
+# print(person['fruits']['name'])
+
+
+# #using del to delete the dict purai nai
+# # del person #person variable purai nai delete hunxa undefined
+# # print(person)
+# #single key-value delete garxa(same kaam of pop and yesari delete)
+# del person['age']
+# del person['fruits']['name']
 # print(person)
 
-#using del to delete the dict purai nai
-# del person
-# print(person)
+#using num as key
+# data = {
+#     1: 'sara',
+#     2: 'paru',
+#     3: 'luxx',
+# }
+# print(data[1]) #1 is key here
 
+# #accesing key,value,both
+# print(data.keys())
+# print(data.values())
+# print(data.items()) #type->dict #forbothkeyandvalue access
+
+#using loop
+
+# for i in data:
+#     print(i)
+#     print(data[i])
+
+# for i in data.items():
+    # print(i)
+
+#to copy one dict to another
+# data2 = data.copy() 
+# #or normally data2 = data (assign garda ni hunxa)
+# print(data2)
+# print(data)
