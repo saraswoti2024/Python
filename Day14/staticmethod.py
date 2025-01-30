409f35838931184f0aeb3b83ddf620e1480a180c
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-01-30 13:49:00 +0545

    day14

--- Day14/staticmethod.py
+++ Day14/staticmethod.py
@@ -5,17 +5,56 @@
 #static method-> aauta default self parameter hunai parxa class vitrw ko value call garna so staticmethod use garyo vanae chahi parena works as both cls,self default
 
 #__init__ wala ma chahi hudaina kna vanae tya hamle attribute define garya hoo
+# class A:
+#     name = "saraswoti"
+
+#     @staticmethod
+#     def initialize_age(age):
+#         print(f"Age initialized to: {age}")
+
+#     @staticmethod
+#     def show(a, b):
+#         print(a + b)
+
+
+# # Use the static methods directly from the class
+# A.initialize_age(22)  # This will print "Age initialized to: 22"
+# A.show(2, 3)          # This will print "5"
+
+# obj1 = A()
+# obj1.show(8,9)
+
+
+#class,static,self example
 class A:
     name = "saraswoti"
+    age = 25
+    
+    def __init__(self,address,d):
+        self.address = address
+        self.d = d
 
-    @staticmethod
-    def initialize_age(age):
+    @classmethod
+    def initialize_age(cls,age):
         print(f"Age initialized to: {age}")
 
     @staticmethod
     def show(a, b):
-        print(a + b)
+        print(A.name)
+        return a+b
+    
+    
+    def display(self):
+        print(self.d)
+        print(self.address)
+        
 
 # Use the static methods directly from the class
 A.initialize_age(22)  # This will print "Age initialized to: 22"
-A.show(2, 3)          # This will print "5"
+# A.show(2, 3)          # This will print "5"
+
+obj1 = A("nepal","dvalue")
+print(obj1.show(8,9))
+
+obj2 = A("shantinagar","obj2dvalue")
+obj2.display()
---
+++ Day14/tasks.py
@@ -0,0 +1,35 @@
+#1.
+# class account:
+#     balance = 20000
+#     account_no = 1234353345
+
+#     def debit(self,amount):
+#         self.balance += amount
+#         print(self.balance)
+    
+#     def credit(self,amount):
+#         self.balance -= amount
+#         print(self.balance)
+
+# account1 = account()
+# account1.debit(1000)
+# account1.credit(200)
+
+#2.
+
+class circle:
+    pi = 3.14
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        print(2*self.pi*self.radius)
+    
+    def circumference(self):
+        print(self.pi*(self.radius**2))
+
+circle1 = circle(5)
+circle1.area()
+circle1.circumference()
+
+
