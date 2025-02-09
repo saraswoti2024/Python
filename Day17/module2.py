cb27b349cdf2172d2f0383ec1f8be5d8fff698e1
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-02-09 19:28:47 +0545

    modules

---
+++ Day17/module2.py
@@ -0,0 +1,24 @@
+# from modules import * ##importing everything
+# import modules  ##importing everything
+# import modules as m ##module name long vayo rey vanae m lae intialize gareko(module ko nickname)
+
+##to import specific part
+# from modules import func,a ##multiple import garda comma(,) use garerw garne
+# from modules import what_is_your_name as name1 ##nickname of variable,function testo rakhna milxa yesari
+# func(4,3)
+# print(a)
+# print(name1)
+
+
+# print(modules.a)
+# modules.func(4,3) ##printutai xa so ya nagareko return gareko vaye teta print lekhthiyam
+
+# print(m.a)
+# m.func(4,3)
+
+##same variable or func in imported and this file
+# import modules
+# a = 12
+# print(modules.a) ##module ma vako print hunxa (dont use from modules import a kna vanae only a use garnu parxa print garda ya jun xa tahi print hunxa not module ko)
+# print(a) #ya vako print hunxa
+
---
+++ Day17/modules.py
@@ -0,0 +1,16 @@
+##two types of module 
+#1.built-in
+#2.user-defined
+
+
+# print(help("math")) ##buit in model haru ma k k xa detail dekhauxa yo print garda
+
+a = 10
+what_is_your_name = "saraswoti"
+name2 = "parbati"
+# print("hi") ##yesari import garda aaru ma ani tyo newfile run garda ya vako print statement ko sabai print hunxa
+def func(a,b):
+    print("addition",a+b)
+    print("subtraction",a-b)
+# func(12,3)
+
