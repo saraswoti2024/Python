f2bb9fd16ce46f512ae81a8c9daa7f3dd12cf72c
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-02-17 23:53:37 +0545

    defination

---
+++ Day17/polymorphism.py
@@ -0,0 +1,7 @@
+##polymorphism means having many forms, aautai method name ko different arguments haru hunxa testai
+##four ways to implement polymorphism:
+#1.duck typing
+#2.method overloading
+#3.operator overloading
+#4.operator overridding
+
